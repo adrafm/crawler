de94166ca2c19957f03cac4cbfb3a756286300ba
Ahmadreza <148530113+ahmdrfm@users.noreply.github.com>
2025-01-09 14:31:36 -0500

    UPDATE: crawler

--- crawler.py
+++ crawler.py
@@ -1,33 +1,44 @@
 import os
 import django
 
-# Setup Django environment
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crawler.settings")
 django.setup()
 
 from products.models import Product
 import json
-
 import requests
 from bs4 import BeautifulSoup
 
 final_products = []
 
-page = requests.get('https://dgtamin.com/product-category/electronic-devices/mobile/mobile-xiaomi/')
+page = requests.get('https://sabzlearn.ir/courses/')
 if page.status_code==200:
     soup = BeautifulSoup(page.text, "html.parser")
 
-    products = soup.find_all('div', attrs={'class': 'products__item-img-color-wrapper'}, limit=5)
+    products = soup.find_all('div', attrs={'class': 'course flex flex-col bg-white dark:bg-darker rounded-xl'}, limit=50)
     for product in products:
-        img = product.find('div', attrs={'class': 'products__item-image-wrapper has_second'}).find('a').find('img', attrs={'class': 'products__item-image entered lazyloaded'})['src']
-        title = product.find('div', attrs={'class': 'products__item-info'}).find('p').find('a')['title']
+        title = product.find('div', attrs={'class': 'flex-grow px-4.5 pt-4 pb-3'}).find('h3').find('a').text
+        img = product.find('a', attrs={'class': 'block h-42'}).find('img')['src']
         final_products.append({"title": title, "img": img})
-    
-    import json
-    with open("products.json", "w", encoding="utf-8") as file:
-        json.dump(final_products, file, ensure_ascii=False, indent=4)
 
-    print("Scraped products:", final_products)
+else:
+    print("Request not Succsess!")
+    print(page.text)
+
+
+page = requests.get('https://maktabkhooneh.org/learn/programming-languages/')
+if page.status_code==200:
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    products = soup.find_all('a', attrs={'class': 'search-course-card flex items-start rounded-lg p-8 md:p-12 bg-white gap-12 sm:gap-16 search-course-card__container-height'}, limit=5)
+    for product in products:
+        title = product.find('div', attrs={'class': 'flex-1 flex flex-col h-full'}).find('div', attrs={'class': 'flex justify-between items-center'}).find('h2').text
+        try:
+            img = product.find('span', attrs={'class': 'relative rounded-md flex aspect-square sm:aspect-9/5 search-course-card__img'}).find('img')['src']
+        except:
+            img = "No Image"
+        final_products.append({"title": title, "img": img})
+
 else:
     print("Request not Succsess!")
     print(page.text)
@@ -41,12 +52,7 @@ if page.status_code==200:
         title = product.find('div', attrs={'class': '_3YTkz_'}).find('a', attrs={'class': '_5PwYIn Sh2LgL'}).text
         img = product.find('div', attrs={'class': 'ssEXuG'}).find('a').find('img')["src"]
         final_products.append({"title": title, "img": img})
-    
-    import json
-    with open("products.json", "w", encoding="utf-8") as file:
-        json.dump(final_products, file, ensure_ascii=False, indent=4)
 
-    print("Scraped products:", final_products)
 else:
     print("Request not Succsess!")
     print(page.text)
